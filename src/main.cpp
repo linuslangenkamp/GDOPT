b70770767579528a1e0a95c8e47e49fdc351c6de
linuslangenkamp <linus@DESKTOP-I2LHO59>
2024-07-22 20:01:06 +0200

    Added hsl solver

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 #include "integrator.h"
 #include "mesh.h"
 #include "gdop.h"
-#include "hypersensitive.h"
+#include "parameterSweep.h"
 #include "solver.h"
 
 using namespace Ipopt;
 
 int main() {
-    Problem problem = createProblem_hypersensitive();
+    Problem problem = createProblem_parameterSweep();
     Integrator rk = Integrator::radauIIA(IntegratorSteps::Steps3);
-    Mesh mesh = Mesh::createEquidistantMesh(1000, 100);
+    Mesh mesh = Mesh::createEquidistantMesh(4000, 5);
 
     Solver solver = Solver(new GDOP(std::move(problem), mesh, rk, InitVars::CONST), 0);
     int status = solver.solve();
--- src/solver.cpp
+++ src/solver.cpp
@@ -17,14 +17,18 @@ int Solver::solve() const {
     app->Options()->SetStringValue("mu_strategy", "adaptive");
     app->Options()->SetIntegerValue("print_level", 5);
     app->Options()->SetStringValue("timing_statistics", "yes");
-    // app->Options()->SetStringValue("output_file", "ipopt.out");
+    app->Options()->SetStringValue("linear_solver", "ma97");
+    app->Options()->SetStringValue("hsllib", "/usr/local/lib/libcoinhsl.so.2.2.5");
+
+    app->Options()->SetStringValue("output_file", "ipopt.out");
 
     ApplicationReturnStatus status = app->Initialize();
+
     int iteration = 0;
     bool stopRefinement = false;
     while (iteration <= maxMeshIterations || stopRefinement){
         status = app->OptimizeTNLP(gdop);
-        // get intervals that ve to be refined
+        // get intervals that ve to be refined -> returns interval indices (i)
         // interpolate x and u
         // rebuild gdop -> run OptimizeTNLP again
         iteration++;
